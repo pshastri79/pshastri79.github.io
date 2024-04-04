2851c830fa546ea0a23debc776575731de4f9540
pshastri79 <66788630+pshastri79@users.noreply.github.com>
2024-04-04 12:07:45 -0700

    update redditsentiment.py

--- redditsentiment.py
+++ redditsentiment.py
@@ -21,4 +21,29 @@ for post in subreddit_stocks.hot(limit=5):
         if counter == 2:
             break
 
+def get_titles_and_comments(subreddit="stocks",limit=5,skip_rate=2):
+    subreddit_out = reddit.subreddit(subreddit)
+    
+    title_and_comments = {}
+    counter = 0
+    
+    for counter, post in enumerate(subreddit_out.hot(limit=limit)):
+        if counter < skip_rate:
+            continue
+        counter += (1-skip_rate)
+        title_and_comments[counter] = ""
+        submission = reddit.submission(post.id)
         
+        title = post.title
+        title_and_comments[counter] = "Title:" +title+ "\n\n"
+        title_and_comments[counter]  = "Comments" +"\n\n"
+        comment_counter = 0
+        for comment in submission.comments:
+            if not comment.body == "[deleted]":
+                title_and_comments[counter] += comment.body
+                comment_counter += 1
+            if comment_counter > 3:
+                break
+    return(title_and_comments)
+                
+print(get_titles_and_comments(subreddit="stocks"))
