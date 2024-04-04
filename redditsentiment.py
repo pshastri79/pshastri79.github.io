6faa565b4b8ebd72bb6f955abd3a937717a0edca
pshastri79 <66788630+pshastri79@users.noreply.github.com>
2024-04-04 09:29:02 -0700

    update redditsentiment.py

--- redditsentiment.py
+++ redditsentiment.py
@@ -7,3 +7,18 @@ reddit = praw.Reddit(client_id=os.getenv("REDDIT_CLIENT_ID"),client_secret=os.ge
                      user_agent="sentiment analysis test")
 for submission in reddit.subreddit("technology").hot(limit=5):
     print(submission.title)
+    
+subreddit_stocks = reddit.subreddit("stocks")
+subreddit_stocks.accounts_active
+
+for post in subreddit_stocks.hot(limit=5):
+    print(post.title)
+    stock_sub = reddit.submission(post.id)
+    counter = 0
+    for subm in stock_sub.comments:
+        print(subm)
+        counter +=1
+        if counter == 2:
+            break
+
+        
