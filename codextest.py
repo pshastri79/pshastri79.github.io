a4eeb2f4d9de775446ef87d566c2caf6b06e5202
pshastri79 <66788630+pshastri79@users.noreply.github.com>
2024-04-04 15:13:46 -0700

    Updated codextest.py

--- codextest.py
+++ codextest.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import inspect
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.organization="org-jDhRxh2NbGke5U3xiiu823zz"
@@ -16,15 +17,33 @@ def docstring_prompt(code):
     prompt = f"{code}\n # A high quality python doc string of the above function.:\n  \"\"\""
     return(prompt)
 
-import inspect
-prompt = docstring_prompt(inspect.getsource(hello))
+
+
+def merge_docstring_and_function(orig_function, docstring):
+    function_string = inspect.getsource(orig_function)
+    split_parts = function_string.split("\n")
+    first_part = split_parts[0]
+    second_part = split_parts[1:]
+    
+    prompt = docstring_prompt(function_string)
 
     
-response = openai.Completion.create(model="gpt-3.5-turbo-instruct", 
+    response = openai.Completion.create(model="gpt-3.5-turbo-instruct", 
                                     prompt=prompt, 
                                     temperature = 0, 
                                     max_tokens=100, 
                                     top_p=1.0, 
                                     stop=["\"\"\""])
-print(response['choices'][0]['text'])
+    docstring = response['choices'][0]['text']
+    output_function = "".join(first_part)+ "\n" + "\"\"\"\n" +docstring+"\"\"\"\n"+ "\n".join(second_part) 
+    
+    print(output_function)
+    
+def hello_v2(name, pet):
+    print(f"Hello {name}\n")
+    print(f"feed {pet}\n")
+    print("All done\n")
+    
+    
 
+merge_docstring_and_function(hello_v2, " ")    
