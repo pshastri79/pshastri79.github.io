902453a0da3c31c5d8b14eee4cd8ee6f89cf5cc0
pshastri79 <66788630+pshastri79@users.noreply.github.com>
2025-09-02 14:45:56 -0700

    Update websiteparser_using_openai.py
    Made changes to include summarize functions for the website parser

--- websiteparser_using_openai.py
+++ websiteparser_using_openai.py
@@ -35,6 +35,34 @@ class Website:
             irrelevant.decompose()
         self.text = soup.body.get_text(separator="\n", strip=True)
 
-ed = Website("https://priyashastri.wixsite.com/priyas")
-print(ed.title)
-print(ed.text)
+# ed = Website("https://priyashastri.wixsite.com/priyas")
+# print(ed.title)
+# print(ed.text)
+system_prompt = "You are an assistant that analyzes the contents of a website \
+and provides a short summary, ignoring text that might be navigation related. \
+Respond in markdown."
+def user_prompt_for(website):
+    user_prompt = f"You are looking at a website titled {website.title}"
+    user_prompt += "\nThe contents of this website is as follows; \
+please provide a short summary of this website in markdown. \
+If it includes news or announcements, then summarize these too.\n\n"
+    user_prompt += website.text
+    return user_prompt
+print(user_prompt_for(ed))
+def messages_for(website):
+    return [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_for(website)}
+    ]
+def summarize(url):
+    website = Website(url)
+    response = openai.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages_for(website)
+    )
+    return response.choices[0].message.content
+def display_summary(url):
+    summary = summarize(url)
+    display(Markdown(summary))
+
+display_summary("https://priyashastri.wixsite.com/priyas")
