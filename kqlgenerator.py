f41d9c556a53639df20e5316d009194a3c570f87
pshastri79 <66788630+pshastri79@users.noreply.github.com>
2025-09-05 09:39:32 -0700

    Create kqlgenerator.py

---
+++ kqlgenerator.py
@@ -0,0 +1,24 @@
+import os
+import openai
+from openai import OpenAI
+import requests
+from bs4 import BeautifulSoup
+from IPython.display import Markdown, display
+
+              
+
+if api_key: 
+    print("Success!")
+else:
+    print("Failure")
+openai = OpenAI()
+
+system_message = "You are an assistant that is great at KQL query generation"
+user_prompt = "Give a KQL query to read 100 records the from azdailyamortizedconsumption table."
+prompts = [
+    {"role": "system", "content": system_message},
+    {"role": "user", "content": user_prompt}
+  ]
+
+response = openai.chat.completions.create(model="gpt-4o-mini", messages=prompts)
+print(response.choices[0].message.content)
